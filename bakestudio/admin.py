8ba315fc6b4784993090d35ea05a50988b5a3cef
Eric <errkkgeorge+github@gmail.com>
2012-03-19 21:43:22 +0000

    more django stuff

---
+++ bakestudio/admin.py
@@ -0,0 +1,7 @@
+from models import Products, BakeDay, Rating
+
+from django.contrib import admin
+
+admin.site.register(Products)
+admin.site.register(BakeDay)
+admin.site.register(Rating)
---
+++ bakestudio/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from datetime import datetime, timedelta
+
+
+class Product(models.Model):
+	description = models.CharField(max_length=500)
+
+	class Meta(self):
+		verbose_name= 'Baked thing'
+
+class BakeDay(models.Model):
+	date = models.DateTimeField()
+	product = models.ForeignKey(Product)
+	user = models.ForeignKey(User, unique_for_date='date')
+
+
+class Rating(models.Model):
+	user = models.ForeignKey(User)
+	product = models.ForeignKey(Product)
+	rating = models.IntegerField(max_length=1)
