34c0b959600e206bbe78924a8e5919bf462b6ab2
Eric <errkkgeorge+github@gmail.com>
2012-04-06 11:20:31 +0100

    Sending resized images over to s3 then delete from the file system

--- bakestudio/bakeweds/models.py
+++ bakestudio/bakeweds/models.py
@@ -2,8 +2,15 @@ from django.db import models
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from datetime import datetime, timedelta
+from django.conf import settings
 
 from PIL import Image
+
+import mimetypes
+import os.path
+import sys
+import S3
+import uuid
 
 
 
@@ -17,7 +24,26 @@ def is_accectable_file(filename):
 		return False
 
 
+def put_s3( filename ):
+	conn = S3.AWSAuthConnection(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
+	
+	filedata = open(filename, 'rb').read()
+	content_type = mimetypes.guess_type(filename)[0]
+	
+	if not content_type:
+		content_type = 'text/plain'
 
+	just_the_filename = 'thumbs/%s' % filename.split('/')[-1]
+
+	return conn.put(settings.AWS_STORAGE_BUCKET_NAME, just_the_filename, S3.S3Object(filedata),
+		{'x-amz-acl': 'public-read', 'Content-Type': content_type})
+
+def remove_s3( filename ):
+	conn = S3.AWSAuthConnection(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
+	
+	just_the_filename = 'thumbs/%s' % filename.split('/')[-1]
+
+	return conn.delete(settings.AWS_STORAGE_BUCKET_NAME, just_the_filename)
 
 
 
@@ -40,15 +66,45 @@ class Product(models.Model):
 
 	def save(self, *args, **kwargs):
 		if is_accectable_file(self.thumbnail.name):
+			
+			try:
+				# remove old image from amazon
+				old_instance = Product.objects.get(pk=self.id)
+				if old_instance.thumbnail.path:
+					remove_s3( str(old_instance.thumbnail.path ) )
+			except:
+				pass
+
+			# Save this one
 			super(Product, self).save(*args,**kwargs)
+			
+			# resize on file system
 			size = 200, 200
 			filename = str(self.thumbnail.path)
 			image = Image.open(filename)
 			image.thumbnail(size, Image.ANTIALIAS)
 			image.save(filename)
+			
+			# send to amazon and remove from local file system
+			if put_s3(filename):
+				os.remove(filename)
+			else:
+				return
+			
+
 			return True
 		else:
 			return False
+
+
+	def prepare_delete(self):
+		filename = str(self.thumbnail.path)
+		remove_s3(filename)
+		return
+
+	def delete(self):
+		self.prepare_delete()
+		super(Product, self).delete()
 
 
 
